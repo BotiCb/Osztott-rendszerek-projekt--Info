32252341468f791c9d4e356c7e2f85300fbf1b78
Demeter-David <148000893+Demeter-David@users.noreply.github.com>
2025-04-02 11:34:05 +0300

    Add display page for updated data in real time

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: '*', // Állítsd be a frontend URL-t
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -14,8 +16,10 @@ async function bootstrap() {
       whitelist: true, // Remove properties not in the DTO
       forbidNonWhitelisted: true, // Throw error if non-whitelisted properties are found
       transform: true, // Transform payloads to DTO instances
-    })
+    }),
   );
+  // Serve static files using ServeStaticModule
+  app.useStaticAssets(join(__dirname, '..', 'public')); // Serve static files
 
   await app.listen(3000);
 }
