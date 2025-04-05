8e877b976a1adeca98d45f480c6a7f3574b47222
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2025-04-05 21:01:20 +0300

    update with basic connection things and added typeorm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,22 @@
 import { Module } from '@nestjs/common';
 import { FormModule } from './form/form.module';
 import { SzavazoRendszerMongooseModule } from './shared/modules/szavazo-rendszer-mongoose-module/szavazo-rendszer-mongoose.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   imports: [
     // Globális mongoose kapcsolat
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres-server-45.postgres.database.azure.com', // or your remote IP
+      port: 5432,
+      username: 'postgres',
+      password: 'szavazo1?',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // DON'T use in prod
+    }),
     SzavazoRendszerMongooseModule,
     // Form funkciók
     FormModule,
